31a77abfc6b4750ed15445c0a9d6c9e1d58e41d5
Yilin <1012455587@qq.com>
2021-01-18 15:51:18 +0800

    添加全局样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,29 @@
 <template>
-<nav>
-  <router-link to="/money" class="item">
-    <Icon name="money"></Icon>
-    记账</router-link>
-  <router-link to="/labels" class="item">
-    <Icon name="labels"></Icon>
-    标签</router-link>
-  <router-link to="/Statistics" class="item">
-    <Icon name="Statistics"></Icon>
-    统计</router-link>
-</nav>
+  <nav>
+    <router-link to="/money" class="item" active-class="selected animate__headShake">
+      <Icon name="money"></Icon>
+      记账
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected animate__headShake">
+      <Icon name="labels"></Icon>
+      标签
+    </router-link>
+    <router-link to="/Statistics" class="item" active-class="selected animate__headShake">
+      <Icon name="Statistics"></Icon>
+      统计
+    </router-link>
+  </nav>
 
 </template>
 
 <script>
+
 import "@/assets/icons/labels.svg"
 import "@/assets/icons/Statistics.svg"
 
 
 export default {
-name: "Nav"
+  name: "Nav"
 }
 </script>
 
@@ -29,17 +33,26 @@ nav {
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   flex-direction: row;
   font-size: 12px;
+
   > .item {
+    color: darksalmon;
     padding: 2px 0;
     width: 33.33333%;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
+
     .icon {
       width: 32px;
       height: 32px;
     }
+
+  }
+  > .item.selected {
+    animation-duration: 800ms; //动画执行时间
+    animation-iteration-count: 1; //动画循环次数
+    color: coral;
   }
 
 }
