b5448af822232428825cb8841e86e1633060261f
Yilin <1012455587@qq.com>
2021-01-19 16:38:59 +0800

    Types组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,211 +1,27 @@
 <template>
-  <Layout  class-prefix="layout">
-    <div class="numberPad">
-      <div class="output">100</div>
-      <div class="buttons">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button class="ok">ok</button>
-        <button class="zero">0</button>
-        <button>.</button>
-      </div>
-    </div>
-    <div>
-      <ul class="types">
-        <li class="selected">支出</li>
-        <li>收入</li>
-      </ul>
-    </div>
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="在这里输入备注">
-      </label>
-    </div>
-
-    <div class="tags">
-      <div class="new">
-        <button>新增标签</button>
-      </div>
-      <ul class="current">
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
-      </ul>
-    </div>
+  <Layout class-prefix="layout">
+    <NumberPad/>
+    <Types/>
+    <Notes/>
+    <Tags/>
   </Layout>
-
 
 </template>
 
 <script>
+import NumberPad from "@/components/Money/NumberPad";
+import Types from "@/components/Money/Types";
+import Notes from "@/components/Money/Notes";
+import Tags from "@/components/Money/Tags";
+
 export default {
-  name: "Money"
+  name: "Money",
+  components: {Tags, Notes, Types, NumberPad}
 }
 </script>
 <style lang="scss">
 .layout-content {
   display: flex;
   flex-direction: column-reverse;
-}
-</style>
-<style lang="scss" scoped>
-@import "~@/assets/style/helper.scss";
-
-.numberPad {
-
-
-  .output {
-    @extend %innerShadow;
-    font-size: 36px;
-    font-family: Consolas, monospace;
-    padding: 9px 16px;
-    text-align: right;
-
-  }
-
-  .buttons {
-    @extend %clearFix;
-
-    > button {
-      float: left;
-      width: 25%;
-      height: 64px;
-      background: transparent;
-      border: none;
-
-      &.ok {
-        float: right;
-        height: 64*2px;
-      }
-
-      &.zero {
-        width: 25*2%;
-      }
-
-      $bg: #FBE5DD;
-
-      &:nth-child(1) {
-        background: $bg;
-      }
-
-      &:nth-child(2), &:nth-child(5) {
-        background: darken($bg, 3%);
-      }
-
-      &:nth-child(3), &:nth-child(6), &:nth-child(9) {
-        background: darken($bg, 6%);
-      }
-
-      &:nth-child(4), &:nth-child(7), &:nth-child(10) {
-        background: darken($bg, 9%);
-      }
-
-      &:nth-child(8), &:nth-child(11), &:nth-child(13) {
-        background: darken($bg, 12%);
-      }
-
-      &:nth-child(14) {
-        background: darken($bg, 15%);
-      }
-
-      &:nth-child(12) {
-        background: darken($bg, 18%);
-      }
-    }
-  }
-}
-
-.types {
-  background: rgba(241, 144, 102, 0.65);
-  display: flex;
-  text-align: center;
-  font-size: 24px;
-
-  > li {
-    width: 50%;
-    line-height: 64px;
-    height: 64px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-
-    &.selected::after {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 4px;
-      background: rgba(231, 127, 103);
-    }
-  }
-}
-
-.notes {
-  font-size: 14px;
-  background: rgba(243, 166, 131, 0.55);
-  padding-left: 16px;
-  display: flex;
-  align-items: center;
-
-  .name {
-    padding-right: 16px;
-  }
-
-  input {
-    height: 64px;
-    flex-grow: 1;
-    border: none;
-    background: transparent;
-    padding-right: 16px;
-  }
-}
-
-.tags {
-  display: flex;
-  flex-direction: column-reverse;
-  flex-grow: 1;
-  font-size: 14px;
-  padding: 16px;
-
-  > .current {
-    display: flex;
-    flex-wrap: wrap;
-
-    > li {
-      background: #f3a683;
-      $h: 24px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 16px;
-      margin-right: 12px;
-      margin-top: 4px;
-    }
-  }
-
-  > .new {
-    padding-top: 16px;
-
-    button {
-      background: transparent;
-      border: none;
-      color: #ed556a;
-      border-bottom: 1px solid;
-      padding: 0 4px;
-    }
-  }
 }
 </style>
