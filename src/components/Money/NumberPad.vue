d195b4b63f532d3d4428e3d73937016a3531a3b2
Yilin <1012455587@qq.com>
2021-01-20 16:52:12 +0800

    封装出一个model

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -64,7 +64,8 @@ export default class NumberPad extends Vue {
 
   ok() {
     this.$emit('update:value',this.output)
-    console.log(typeof this.output)
+    this.$emit('submit',this.output)
+    this.output="0"
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,39 +1,45 @@
 <template>
   <Layout class-prefix="layout">
-
-    <NumberPad :value.sync="record.amount"/>
-    <Types :value.sync="record.type" />
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
+    <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
-
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
 import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
+import recordListModel from '@/model.ts';
 
-
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number;
-}
+const recordList = recordListModel.fetch();
 @Component({components: {Tags, Notes, Types, NumberPad}})
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
-  record: Record = {tags: [], notes: '', type: '-', amount:0};
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
 
   onUpdateTags(value: string[]) {
-   this.record.tags = value
+    this.record.tags = value;
   }
+
   onUpdateNotes(value: string) {
-    this.record.notes = value
+    this.record.notes = value;
+  }
+
+  saveRecord() {
+    const record2: RecordItem = recordListModel.clone(this.record);
+    record2.createdAt = new Date();
+    this.recordList.push(record2);
+  }
+
+  @Watch('recordList')
+  onRecordListChange() {
+    recordListModel.save(this.recordList);
   }
 }
 
